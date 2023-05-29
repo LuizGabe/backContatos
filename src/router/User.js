a1ebf5dd072fac0cac94ffaa024d83ecbbcff223
LuizGabe <luizgabrielgrupe@gmail.com>
2023-05-28 23:56:36 -0300

    Create function for Users

--- src/router/User.js
+++ src/router/User.js
@@ -1,38 +1,65 @@
 import { Router } from "express";
+import { Database } from "../../database.js";
+import { randomUUID } from "node:crypto"
+
+const database = new Database();
+
+const tableUsers = "users";
 
 const userRoute = Router();
 
 // Rota para listar todos os usuários
-userRoute.get('/All', (req, res) => {
-  res.send('Retorna todos os usuários');
-})
+userRoute.get('/', (req, res) => {
+  const search = req.query.search;
 
-// Rota para acessar o usuário criado por id e nome
-userRoute.get('/search/:busca', (req, res) => {
-  res.send('Busca um usuário');
+  if(!search){
+    res.json(database.select(tableUsers));
+    return
+  }
+
+  console.log(req.query.search);
+  res.send(`Bucando um usuário com a busca: ${search}`);
+});
+userRoute.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json(database.select(tableUsers, id));
 });
 
+// Rota para acessar o usuário criado por id e nome
 userRoute.post('/', (req, res) => {
   const {
     id = '',
-    name = '', 
+    name = '',  
+    office = '',
     coverImg = '', 
     avatarImg = '', 
     stacks = []
   } = req.body;
 
-  if (!id == '') {
-    console.log('Adicionar usuários aqui');
-    res.send('Adicionar usuários aqui');
+  if (id) {
+    database.update(tableUsers, id, { name, office, coverImg, avatarImg, stacks } );
+    res.send({ message: 'Contato atualizado', user: { id, name, office, coverImg, avatarImg, stacks } });
+    return
   }
 
-  console.log({ id, name, coverImg, avatarImg, stacks });
-  res.send('Edita um usuário');
+  const user = {
+    id: randomUUID(),
+    name,
+    office,
+    coverImg,
+    avatarImg,
+    stacks
+  }
+
+  database.insert(tableUsers, user);
+
+  res.status(201).json({ message: 'Contato adicionado', user: user});
 })
 
-// Rota que deleta o usuário
-userRoute.delete('/', (req, res) => {
-  res.send('Deleta um usuário');
+userRoute.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  database.delete(tableUsers, id);
+  res.send({ message: 'Contato deletado', id});
 })
 
 export { userRoute }
