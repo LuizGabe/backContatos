32e18160071539c2679337c4f3ba12247dd3eaa4
LuizGabe <luizgabrielgrupe@gmail.com>
2023-05-29 05:07:33 -0300

    Adding postman file and Option endpoint

--- main.js
+++ main.js
@@ -18,6 +18,23 @@ app.use('/user', userRoute);
 
 app.use('/contact', contactRoute)
 
+app.options('/', (req, res) => {
+	res.status(200).json({
+		message: 'Opções possiveis',
+		user: [
+			{ descricao: 'Volta todos os usuários e por query param uma pesquisa' , protocol: 'GET', domain:'http://127.0.0.1:3000/user'},
+			{ descricao: 'Volta um usuário com o id fornecido por param' , protocol: 'GET', domain:'http://127.0.0.1:3000/user/'},
+			{ descricao: 'Adiciona ou atualiza um usuário' , protocol: 'POST', domain:'http://127.0.0.1:3000/user'},
+			{ descricao: 'Deleta um usuário com o id fornecido por param' , protocol: 'DELETE', domain:'http://127.0.0.1:3000/user/'},
+		],
+		contact: [
+			{ descricao: 'Volta todos os usuários e por query param uma pesquisa' , protocol: 'GET', domain:'http://127.0.0.1:3000/contact'},
+			{ descricao: 'Adiciona ou atualiza um usuário' , protocol: 'POST', domain:'http://127.0.0.1:3000/contact'},
+			{ descricao: 'Deleta um usuário com o id fornecido por param' , protocol: 'DELETE', domain:'http://127.0.0.1:3000/contact/'},
+		]
+	})
+})
+
 app.listen('3000', () => {
 	console.log(`Example app listening on port ${port}!`)
 });
